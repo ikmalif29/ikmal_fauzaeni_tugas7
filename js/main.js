49431595dab831f3da0c86d2b9b9515743d90985
ikmalif29 <ikmalfauzaeni@gmail.com>
2024-07-04 17:05:18 +0700

    asd

---
+++ js/main.js
@@ -0,0 +1,340 @@
+let product = [
+    {
+        id: 1,
+        name: "Laptop Acer Intel Core I3",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i3",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 2,
+        name: "Laptop Acer Aspire A514",
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHkSQY78LiuIZFX88NYdmtiz26xLgNLDuLDQ&s",
+        price: "IDR 6.100.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 3,
+        name: "HP Pavilion 13.3 FHD",
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNPkIzbCliA21Ho9XE-gmP2mHiHwubtJjlww&s",
+        price: "IDR 10.673.783,00",
+        desc: [
+            "Processor: Intel Core i3",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 4,
+        name: " HP Stream 14 - Intel Celeron",
+        gambar: "https://images-cdn.ubuy.co.id/64c41e4771c5f52216163af1-hp-stream-14-laptop-intel-celeron.jpg",
+        price: "IDR 5.499.000,00",
+        desc: [
+            "Processor: Intel Core i3",
+            "Memory: 4GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 5,
+        name: "Vivobook 14X (M1403, AMD Ryzen)",
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRttOvTJd2vKVB7NkvEMkEaT--fSrywPkTXZw&s",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 6,
+        name: "Laptop For Work - All",
+        gambar: "https://www.asus.com/media/Odin/Websites/global/Series/15.png",
+        price: "IDR 8.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 7,
+        name: "ASUS Zenbook DUO (2024) ",
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRigxOwTWBO1LWEb7Sc0HbYvQA3M-9Wn7AdwA&s",
+        price: "IDR 34.999.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 8,
+        name: "ASUS Vivobook 14X OLED",
+        gambar: "https://dlcdnwebimgs.asus.com/gain/c06af139-c10f-41d2-b4c5-1cacb3ec19c5/",
+        price: "IDR 6.300.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 9,
+        name: "Asus ROG Strix G513QC",
+        gambar: "https://images.tokopedia.net/img/cache/700/OJWluG/2022/6/30/b8ae0840-02a2-4c49-be38-9ee4c9d0f6d3.jpg",
+        price: "IDR 9.000.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 10,
+        name: "Gaming ROG",
+        gambar: "https://www.asus.com/media/Odin/Websites/global/ProductLine/20200824120814.jpg",
+        price: "IDR 11.800.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 11,
+        name: "ROG Strix SCAR 16",
+        gambar: "https://id.store.asus.com/media/catalog/product/1/6/16_black_perkey_02_l.jpg?width=439&height=439&store=rog_id_ID&image-type=image",
+        price: "IDR 71.999.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 12,
+        name: "ROG Flow X16 GV601RM",
+        gambar: "https://images.tokopedia.net/img/cache/700/OJWluG/2022/10/11/f36f5fac-85de-49ad-a894-985b5e877054.jpg?ect=4g",
+        price: "IDR 21.563.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 13,
+        name: "ROG Strix G17 (2022)",
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGHin8Qns0NYyBAdVrgkOXGjmHz-rGhYQn6w&",
+        price: "IDR 20.499.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 14,
+        name: "Macbook Apple Air M2 ",
+        gambar: "https://cdn.topsellbelanja.com/wp-content/uploads/2023/08/Laptop-Macbook-Apple-1-c.webp",
+        price: "IDR 16.399.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 15,
+        name: "MacBook Pro (16 inci)",
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKQFj5lUBaoI0o12AxDtsk0PgoNVQOZ8CSSg&s",
+        price: "IDR p32.999.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 16,
+        name: "Apple Macbook Air M1 2020",
+        gambar: "https://images.tokopedia.net/img/cache/700/VqbcmM/2021/2/25/f5ec328e-6ee9-4ebe-94e2-860af25e0d29.jpg",
+        price: "IDR 21.650.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 17,
+        name: "Lenovo Ideapad 5 15itl05 ",
+        gambar: "https://images.tokopedia.net/img/cache/700/VqbcmM/2022/5/30/0414e183-7b7c-4787-9b35-28ca5979654b.png",
+        price: "IDR 11.310.00000",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 18,
+        name: "lenovo ideapad slim 3 ryzen ",
+        gambar: "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/91/MTA-122726430/lenovo_laptop-lenovo-ideapad-slim-3-ryzen-3-3250u_full9.jpg",
+        price: "IDR 5.800.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 19,
+        name: "LENOVO LAPTOP GAMING 3",
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwi3XDXYGfKupx0NwIMKScw_f6YEki2nHjhA&s",
+        price: "IDR 13.299.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+    {
+        id: 20,
+        name: "Lenovo Flex 5G 14Q8CX05 ",
+        gambar: "https://renanstore.co.id/wp-content/uploads/2023/05/75ff78c9-98b3-4b84-a1ae-5ca566da4bb1.jpg",
+        price: "IDR 5.500.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+        ]
+    },
+];
+
+let big = document.querySelector(".big-container")
+big.style.width = "100%"
+big.style.gap = "40px"
+big.style.justifyContent = "center"
+big.style.alignItems = "center"
+big.style.display = "grid"
+big.style.gridTemplateColumns = "1fr 1fr 1fr 1fr"
+
+product.forEach((p) => {
+    let container = document.createElement("div");
+    container.style.backgroundColor = "white"
+    container.style.border = "2px solid black"
+    container.style.width = "300px"
+    container.style.height = "500px"
+    container.style.marginLeft = "10px"
+    container.style.marginTop = "30px"
+    container.style.marginBottom = "30px"
+    container.style.display = "flex"
+    container.style.cursor = "pointer"
+    container.style.justifyContent = "center"
+    container.style.flexDirection = "column"
+    container.style.textAlign = "center"
+    container.style.borderRadius = "50px"
+    container.style.cursor = "pointer"
+    container.style.boxShadow = "5px 5px 20px black" 
+
+    let figure = document.createElement("figure")
+    let figcaption = document.createElement("figcaption")
+    let image = document.createElement("img")
+    image.src = p.gambar;
+    image.style.width = "180px"
+    image.style.height = "180px"
+    let h1 = document.createElement("h1")
+    h1.textContent = p.name
+    let h2 = document.createElement("h2")
+    h2.textContent = p.price
+    h2.style.color = "red"
+    let buy = document.createElement("button")
+    buy.textContent = "beli sekarang"
+    buy.style.margin = "2px"
+    buy.style.backgroundColor = "cyan"
+    let like = document.createElement("button")
+    like.textContent = "like"
+    like.style.margin = "2px"
+    like.style.backgroundColor = "cyan"
+    let coment = document.createElement("button")
+    coment.textContent = "coment"
+    coment.style.margin = "2px"
+    coment.style.backgroundColor = "cyan"
+    let ul = document.createElement("ul")
+    
+    p.desc.forEach((d) => {
+        let li = document.createElement("li")
+        li.textContent = d
+        li.style.color = "blue"
+        ul.appendChild(li)
+    })
+
+    buy.addEventListener("click" , () =>{
+        confirm(`apakah anda akan membeli produk ini ?`)
+    })
+    buy.addEventListener("mouseover" , () =>{
+        buy.style.backgroundColor = "red"
+    })
+    buy.addEventListener("mouseout" , () =>{
+        buy.style.backgroundColor = "cyan"
+    })
+    
+    
+    like.addEventListener("click" , () => {
+        alert(`kamu menyukai produk ini`)
+    })
+    like.addEventListener("mouseover" , () =>{
+        like.style.backgroundColor = "red"
+    })
+    like.addEventListener("mouseout" , () =>{
+        like.style.backgroundColor = "cyan"
+    })
+    
+    coment.addEventListener("click" , () => {
+        prompt(`apa pendapat anda tentang produk ini ?`)
+    })
+    coment.addEventListener("mouseover" , () =>{
+        coment.style.backgroundColor = "red"
+    })
+    coment.addEventListener("mouseout" , () =>{
+        coment.style.backgroundColor = "cyan"  
+    })
+
+    figcaption.appendChild(h1)
+    figcaption.appendChild(h2)
+    figcaption.appendChild(ul)
+    figcaption.appendChild(buy)
+    figcaption.appendChild(like)
+    figcaption.appendChild(coment)
+    figure.appendChild(image)
+    figure.appendChild(figcaption)
+    container.appendChild(figure)
+    big.appendChild(container)
+})
